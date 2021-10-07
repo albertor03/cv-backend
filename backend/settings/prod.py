60afb39240f111e7afc331ca321d2ef5b10e9b36
albertor03 <alberto.zapata.orta@gmail.com>
2021-10-07 12:17:48 -0500

    fix error

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -12,6 +12,7 @@ ROOT_URLCONF = environment
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = f"{BASE_DIR.parent}/media/"
 MEDIA_ROOT = BASE_DIR.parent / 'media'
