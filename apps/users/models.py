9e2c956fda1c48a2575cdebf5f23a702b0bdc342
albertor03 <alberto.zapata.orta@gmail.com>
2021-10-11 15:57:00 -0500

    Update first name field in the user models

--- apps/users/models.py
+++ apps/users/models.py
@@ -4,11 +4,11 @@ from django.contrib.auth.models import BaseUserManager, AbstractUser, Permission
 
 class UserManager(BaseUserManager):
 
-    def _create_user(self, username, email, name, last_name, password, is_staff, is_superuser, **extra_fields):
+    def _create_user(self, username, email, first_name, last_name, password, is_staff, is_superuser, **extra_fields):
         user = self.model(
             username=username,
             email=email,
-            name=name,
+            first_name=first_name,
             last_name=last_name,
             is_staff=is_staff,
             is_superuser=is_superuser,
@@ -20,18 +20,18 @@ class UserManager(BaseUserManager):
 
         return user
 
-    def create_user(self, username, email, name, last_name, password=None, **extra_fields):
-        return self._create_user(username, email, name, last_name, password, False, False, **extra_fields)
+    def create_user(self, username, email, first_name, last_name, password=None, **extra_fields):
+        return self._create_user(username, email, first_name, last_name, password, False, False, **extra_fields)
 
-    def create_superuser(self, username, email, name, last_name, password=None, **extra_fields):
-        return self._create_user(username, email, name, last_name, password, True, True, **extra_fields)
+    def create_superuser(self, username, email, first_name, last_name, password=None, **extra_fields):
+        return self._create_user(username, email, first_name, last_name, password, True, True, **extra_fields)
 
 
 class User(AbstractUser, PermissionsMixin):
     _id = models.ObjectIdField()
     username = models.CharField(max_length=255, unique=True)
     email = models.EmailField('Email', max_length=255, unique=True)
-    name = models.CharField('Name', max_length=255, blank=True, null=True)
+    first_name = models.CharField('Name', max_length=255, blank=True, null=True)
     last_name = models.CharField('Name', max_length=255, blank=True, null=True)
     is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
@@ -47,10 +47,10 @@ class User(AbstractUser, PermissionsMixin):
         db_table = 'users'
 
     USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['email', 'name', 'last_name']
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
 
     def natural_key(self):
         return self.username
 
     def __str__(self):
-        return f"{self.name} {self.last_name}"
+        return f"{self.first_name} {self.last_name}"
