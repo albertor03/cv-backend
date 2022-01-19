85769a368f18b1387fd880526a20ff9288288e18
albertor03 <alberto.zapata.orta@gmail.com>
2022-01-19 11:48:46 -0500

    Update urls.py

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 
 from rest_framework.urlpatterns import format_suffix_patterns
-from rest_framework_simplejwt.views import (
-    TokenRefreshView,
-)
 
 from .views import (
     SingUpUserApiView,
@@ -14,7 +11,6 @@ from .views import (
 )
 
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
 )
 
@@ -24,8 +20,6 @@ urlpatterns = [
     path('sing-up/', SingUpUserApiView.as_view(), name='user_create'),
     path('login/', LoginAPIView.as_view(), name='login'),
     path('logout/', LogoutAPIView.as_view(), name='logout'),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
