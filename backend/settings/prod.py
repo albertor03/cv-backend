3fe492f5cd6f16f5bb030ca2fc4cfc2383bb858f
albertor03 <alberto.zapata.orta@gmail.com>
2022-01-30 13:29:42 -0500

    fix static path

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -10,15 +10,9 @@ ROOT_URLCONF = environment
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find staticfiles files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
-)
-# STATIC_URL = '/staticfiles/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 MEDIA_URL = f"{BASE_DIR.parent}/media/"
 MEDIA_ROOT = BASE_DIR.parent / 'media/'
