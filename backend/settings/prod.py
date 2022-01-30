4a4db2831562a5a871207bab0e2d3cc65924b521
albertor03 <alberto.zapata.orta@gmail.com>
2022-01-30 13:18:34 -0500

    Change name of static folder

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -10,9 +10,15 @@ ROOT_URLCONF = environment
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/staticfiles/'
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# Extra places for collectstatic to find staticfiles files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'staticfiles'),
+)
+# STATIC_URL = '/staticfiles/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 MEDIA_URL = f"{BASE_DIR.parent}/media/"
 MEDIA_ROOT = BASE_DIR.parent / 'media/'
