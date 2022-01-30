51482d873fd027d634815d4f218f4c4c9269696c
albertor03 <alberto.zapata.orta@gmail.com>
2022-01-30 13:05:39 -0500

    Update prod.py

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -12,10 +12,10 @@ ROOT_URLCONF = environment
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = f"{BASE_DIR.parent}/media/"
-MEDIA_ROOT = BASE_DIR.parent / 'media'
+MEDIA_ROOT = BASE_DIR.parent / 'media/'
 
 DATABASES = {
   'default': {
