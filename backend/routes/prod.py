d63ce9531e537ffc47216d89fc5dd13d2aa5eb9e
albertor03 <alberto.zapata.orta@gmail.com>
2022-02-04 19:17:11 -0500

    Course endpoints added

--- backend/routes/prod.py
+++ backend/routes/prod.py
@@ -39,6 +39,7 @@ urlpatterns = [
     path('api/personal-info/', include('apps.information.urls')),
     path('api/jobs/', include('apps.job.urls')),
     path('api/educations/', include('apps.education.urls')),
+    path('api/courses/', include('apps.course.urls')),
 
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
