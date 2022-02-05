e6d349c51e70f869d9113f846376788efeec7565
albertor03 <alberto.zapata.orta@gmail.com>
2022-02-04 19:53:47 -0500

    Skill test created

--- apps/skill/tests.py
+++ apps/skill/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from rest_framework import status
+from ..tests_folder.set_up import TestUserSetUp, generate_skill
+
+
+class CreateSkillTestCases(TestUserSetUp):
+    def test_01_create_skill(self):
+        skill = generate_skill()
+        resp = self.client.post(reverse('list_create_skill'), skill)
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(resp.data['data']['name'], skill['name'])
+        self.assertEqual(resp.data['data']['percentage'], skill['percentage'])
+        self.assertFalse(resp.data['errors'])
