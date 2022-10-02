667553db1c8990600a290c3945ce7cb754268b3c
Alberto <alberto.zapata.orta@gmail.com>
2022-10-02 12:57:37 -0500

    fix ENV variable in asgi.py wsgi.py and manage.py files

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'backend.settings.{os.environ["ENV"]}')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'backend.settings.{os.getenv("ENV", "prod")}')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
