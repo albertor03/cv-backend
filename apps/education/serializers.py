6bf576fc5448a21167c47d273657fda71a18f08c
Alberto <alberto.zapata.orta@gmail.com>
2023-08-29 20:13:51 -0500

    Education model and serializer were refactored

--- apps/education/serializers.py
+++ apps/education/serializers.py
@@ -1,4 +1,7 @@
+import base64
+
 from bson import ObjectId
+from rest_framework.exceptions import NotFound
 
 from .models import EducationModels
 
@@ -7,32 +10,25 @@ from rest_framework import serializers
 
 class EducationSerializer(serializers.ModelSerializer):
 
+    def get_query(self, pk):
+        data = EducationModels.objects.filter(_id=ObjectId(pk)).first()
+        if not data:
+            raise NotFound({'data': {}, 'errors': ['Information not found.']})
+        return data
+
     class Meta:
         model = EducationModels
         fields = '__all__'
 
+    def validate(self, attrs):
+        if 'end_date' in attrs and attrs['end_date'] is not None:
+            if attrs['end_date'] < attrs['start_date']:
+                raise serializers.ValidationError(detail={'data': {}, 'errors': ['The end date field must not be less than the start date field.']})
 
-class UpdateEducationSerializer(serializers.ModelSerializer):
+        if 'certificate' in attrs and self.context['request'].method != 'PATCH':
+            try:
+                base64.b64decode(attrs['certificate'], validate=True)
+            except:
+                raise serializers.ValidationError(detail={'data': {}, 'errors': ['The certificate field must be base64.']})
 
-    class Meta:
-        model = EducationModels
-        fields = '__all__'
-
-    def update(self, instance, validated_data):
-        education = self.Meta.model.objects.filter(_id=ObjectId(instance.pk)).first()
-        if education.certificate.name:
-            education.certificate.delete(education.certificate.name)
-
-        for attr, value in validated_data.items():
-            setattr(instance, attr, value)
-
-        instance.save()
-
-        return instance
-
-
-class CreateEducationSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = EducationModels
-        exclude = ['created_at', 'updated_at']
+        return attrs
